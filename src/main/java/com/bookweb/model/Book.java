a07d195ef39c674e09733ad1fe2e1743a54e68a1
npaxzz <82027125+npaxzz@users.noreply.github.com>
2025-09-16 15:41:14 +0700

    get set

--- src/main/java/com/bookweb/model/Book.java
+++ src/main/java/com/bookweb/model/Book.java
@@ -40,4 +40,36 @@ public class Book {
 	public void setId(Long id) {
 		this.id = id;
 	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
 }
--- src/main/java/com/bookweb/model/Category.java
+++ src/main/java/com/bookweb/model/Category.java
@@ -12,4 +12,27 @@ public class Category {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String name;
+
+	public Category() {
+	}
+
+	public Category(String name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setIdName(String name) {
+		this.name = name;
+	}
 }
