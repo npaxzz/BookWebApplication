1669a5d3d5712392e088b7a264f37d71067542e3
KITTICHAI-WANNAPRUEK <kittichai.wa@kkumail.com>
2025-09-23 04:55:29 +0700

    Update ReviewRepository.java
    update ReviewRepository

--- src/main/java/com/bookweb/repository/ReviewRepository.java
+++ src/main/java/com/bookweb/repository/ReviewRepository.java
@@ -1,5 +1,14 @@
 package com.bookweb.repository;
 
-public class ReviewRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bookweb.model.Review;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+	List<Review> findByBookId(Long bookId);
+	List<Review> findByBookRating(int rating);
 }
